ecc0408578b173a9dd9276ec63bd219224af5d0e
David Sosa Valdes <david.sosa.valdes@gmail.com>
2016-04-19 17:46:43 -0500

    Add bower components

--- config/attire.php
+++ config/attire.php
@@ -18,3 +18,25 @@
 |
 */
 $config['global_vars']['system']['fullname'] = 'EntrustBundle';
+
+/*
+|--------------------------------------------------------------------------
+| Sprockets-PHP - Assets Paths
+|--------------------------------------------------------------------------
+|
+| The asset paths are divided by "modules" to be as flexible as it can.
+|
+| You have 2 keys in each modules: 
+|	- Directories: which list directories where the Pipeline must search 
+|		files.
+|	- Prefixes: which will append the path for the extension to the 
+|		directory (ie a js file will get javascripts/ appended to its paths).
+|	
+| Set the external directories where the pipeline can find the bower 
+| components, composer packages, etc.
+|
+*/
+$config['pipeline_paths']['external']['directories'] = array(
+	FCPATH.'bower_components/',
+	BUNDLEPATH.'EntrustBundle/assets/bower_components/'
+);
