9a473fc5ded4677cc0e110680fb5460885b79434
David Sosa Valdes <david.sosa.valdes@gmail.com>
2016-05-02 23:16:35 -0500

    Ignore and fix some files

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,5 @@ $route['entrust/logout']            = 'login/logout';
 $route['entrust/users']             = 'user/index';
 $route['entrust/api/cerberus/(.+)'] = 'cerberus/$1';
 
-$route['default_controller']        = 'dashboard';
-
 /* End of file routes.php */
 /* Location: .bundles/EntrustBundle/config/routes.php */
